7e5b26a261924019a541e39e2bd6f855d15e4c3d
Max <214122627+Diplomoceras-maximum@users.noreply.github.com>
2025-06-21 15:49:42 +0100

    Add explanation of events in javascript

---
+++ script.js
@@ -0,0 +1,10 @@
+// Events
+
+// Events can make changes to the DOM dynamically or on demand
+// Events are actions that occur on webpages, such as mouse clicks or key presses
+// Javascript can be used to make the webpage "listen" to and react to these events
+
+// Three ways to do this:
+// Specify function attibutes directly in the HTML elements
+// Set properties on the DOM noes in javascript (onclick , onmousedown, etc)
+// Attach event listeners to the DOM nodes in javascript
